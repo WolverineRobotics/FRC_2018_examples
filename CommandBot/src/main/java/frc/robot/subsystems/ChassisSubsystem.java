294ac976ab53580e8cd291c0c8ef5ece08440a45
Khalil Balde <KHALIL12345385@gmail.com>
2018-11-02 21:56:26 -0400

    Added Chassis Subsystem created basic chassis subsystem with motrs and encoders

---
+++ CommandBot/src/main/java/frc/robot/subsystems/ChassisSubsystem.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+/*SECTION Subsystems
+* Like on the robot, we have specific subsystems for each subsystem on the robot ex. Shooter, Drive, Lift, Feeder etc.
+* The subsystem holds all the getters and setters for that subsystem
+* 
+//!SECTION */
+public class ChassisSubsystem extends Subsystem{
+    private static Spark leftMotor1 = new Spark(RobotMap.LEFT_MOTOR_1);
+    private static Spark leftMotor2 = new Spark(RobotMap.LEFT_MOTOR_2);
+    private static Spark rightMotor1 = new Spark(RobotMap.RIGHT_MOTOR_1);
+    private static Spark rightMotor2 = new Spark(RobotMap.RIGHT_MOTOR_2);
+
+    private static Encoder leftEncoder = new Encoder(RobotMap.LEFT_ENCODER_A, RobotMap.LEFT_ENCODER_B);
+    private static Encoder rightEncoder = new Encoder(RobotMap.RIGHT_ENCODER_A, RobotMap.RIGHT_ENCODER_B);
+
+    public ChassisSubsystem(){
+        rightMotor1.setInverted(true);
+        rightMotor2.setInverted(true);
+
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+    }
+
+    public void setLeftSpeed(double speed){
+        leftMotor1.setSpeed(speed);
+        leftMotor2.setSpeed(speed);
+    }
+
+    public void setRightSpeed(double speed){
+        rightMotor1.setSpeed(speed);
+        rightMotor2.setSpeed(speed);
+    }
+
+    public double getLeftSpeed(){
+        return rightEncoder.getRate();
+    }
+
+    public double getRightSpeed(){
+        return rightEncoder.getRate();
+    }
+
+    public double getLeftDistance(){
+        return leftEncoder.getDistance();
+    }
+
+    public double getRightDistance(){
+        return rightEncoder.getDistance();
+    }
+}
